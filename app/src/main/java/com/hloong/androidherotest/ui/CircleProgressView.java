e0f298e997b169c5b44b8d875133f0744cd9e35d
hloong <hbxglong@163.com>
2016-04-09 14:32:26 +0800

    test

---
+++ app/src/main/java/com/hloong/androidherotest/ui/CircleProgressView.java
@@ -0,0 +1,38 @@
+package com.hloong.androidherotest.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hl on 16/4/8.
+ */
+public class CircleProgressView extends View {
+    private Paint mCircle;
+
+    public CircleProgressView(Context context) {
+        super(context);
+    }
+
+    public CircleProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//        mCircle = length/2;
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
