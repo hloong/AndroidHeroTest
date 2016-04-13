66302df1b8e7ad047030a5f2e5d5b7c69019a133
Administrator <hbxglong@163.com>
2016-04-13 16:16:47 +0800

    增加一个圆形的绘制

--- app/src/main/java/com/hloong/androidherotest/MainActivity.java
+++ app/src/main/java/com/hloong/androidherotest/MainActivity.java
@@ -2,12 +2,18 @@ package com.hloong.androidherotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.hloong.androidherotest.ui.CircleProgressView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CircleProgressView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = (CircleProgressView) findViewById(R.id.cv);
+        view.setSweepValue(50);
     }
 }
--- app/src/main/java/com/hloong/androidherotest/ui/CircleProgressView.java
+++ app/src/main/java/com/hloong/androidherotest/ui/CircleProgressView.java
@@ -3,6 +3,7 @@ package com.hloong.androidherotest.ui;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,9 +13,21 @@ import com.hloong.androidherotest.util.AppUtil;
  * Created by hl on 16/4/8.
  */
 public class CircleProgressView extends View {
-    private Paint mCircle;
-    private int mCircleXY;
+    private int mMeasureH;
+    private int mMeasureW;
+
     private float mRadius;
+    private float mCircleXY;
+    private float mSweepAngle;
+    private float mShowTextSize;
+    private float mSweepValue = 66;
+
+    private Paint mCircle;
+    private Paint mArcPaint;
+    private RectF mArcRectF;
+    private Paint mTextPaint;
+
+    private String mShowText;
 
     public CircleProgressView(Context context) {
         super(context);
@@ -31,12 +44,67 @@ public class CircleProgressView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mCircleXY = AppUtil.getWidth(getContext())/2;
-        mRadius = (float) (AppUtil.getWidth(getContext()) * 0.5 / 2);
+        canvas.drawCircle(mCircleXY,mCircleXY,mRadius,mCircle);
+        canvas.drawArc(mArcRectF,270,mSweepAngle,false,mArcPaint);
+        canvas.drawText(mShowText,0,mShowText.length(),mCircleXY,
+                mCircleXY + (mShowTextSize /4),mTextPaint);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mMeasureW = MeasureSpec.getSize(widthMeasureSpec);
+        mMeasureH = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(mMeasureW,mMeasureH);
+        initView();
+
+    }
+
+    private void initView() {
+        float length = mMeasureH >= mMeasureW ? mMeasureW : mMeasureH;
+        mCircleXY = length / 2;
+        mRadius = (float)(length * 0.5 /2);
+        mCircle = new Paint();
+        mCircle.setAntiAlias(true);
+        mCircle.setColor(getResources().getColor(android.R.color.holo_blue_bright));
+
+        mArcRectF = new RectF((float) (length * 0.1), (float) (length * 0.1),
+                (float) (length * 0.9),(float) (length * 0.9));
+        mSweepAngle = (mSweepValue / 100f) * 360f;
+
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setColor(getResources().getColor(android.R.color.holo_blue_bright));
+        mArcPaint.setStrokeWidth((float)(length * 0.1));
+        mArcPaint.setStyle(Paint.Style.STROKE);
+
+        mShowText = setShowText();
+        mShowTextSize = setShowTextSize();
+        mTextPaint = new Paint();
+        mTextPaint.setTextSize(mShowTextSize);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    private String setShowText(){
+        this.invalidate();
+        return "Android Skill";
+    }
+
+    private float setShowTextSize() {
+        this.invalidate();
+        return 50;
+    }
+
+    public void forceInvalidate() {
+        this.invalidate();
+    }
+
+    public void setSweepValue(float sweepValue){
+        if (sweepValue != 0){
+            mSweepValue = sweepValue;
+        }else{
+            mSweepValue = 25;
+        }
+        this.invalidate();
     }
 }
