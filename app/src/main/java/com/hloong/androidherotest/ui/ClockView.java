022ff938456ce419300125136a3ec0c11b9f192f
Administrator <hbxglong@163.com>
2016-05-05 17:13:36 +0800

    绘制时钟

--- app/src/main/java/com/hloong/androidherotest/ui/ClockView.java
+++ app/src/main/java/com/hloong/androidherotest/ui/ClockView.java
@@ -2,6 +2,8 @@ package com.hloong.androidherotest.ui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,7 +14,7 @@ import com.hloong.androidherotest.util.DisplayUtil;
  * Created by Administrator on 2016/5/4.
  */
 public class ClockView extends View{
-    private Paint paint,degreePaint,paintHour,paintMinute;
+    private Paint paint,degreePaint,paintHour,paintMinute,paintLayer;
     public ClockView(Context context) {
         super(context);
     }
@@ -52,7 +54,7 @@ public class ClockView extends View{
         paintMinute = new Paint();
         paintMinute.setAntiAlias(true);
         paintMinute.setStrokeWidth(10);
-        canvas.save();
+        canvas.save();//保存画布
         canvas.translate(width/2,height/2);
         canvas.drawLine(0,0,100,100,paintHour);
         canvas.drawLine(0,0,100,200,paintMinute);
@@ -78,7 +80,21 @@ public class ClockView extends View{
                 String degree = ""+i;
                 canvas.drawText(degree,width/2 - degreePaint.measureText(degree),height/2-width/2+60,degreePaint);
             }
-            canvas.rotate(30,width/2,height/2);
+            canvas.rotate(30,width/2,height/2);//旋转画布
         }
+
+        paintLayer = new Paint();
+        paintLayer.setColor(Color.BLUE);
+        canvas.drawCircle(100,100,100,paintLayer);
+        canvas.saveLayerAlpha(0,0,250,250,127,Canvas.FULL_COLOR_LAYER_SAVE_FLAG);
+        paintLayer.setColor(Color.CYAN);
+        canvas.drawCircle(150,150,100,paintLayer);
+        canvas.restore();//合并图层
+
+
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setRotate(0,150);
+        colorMatrix.setRotate(1,150);
+        colorMatrix.setRotate(2,150);
     }
 }
