83e9f1cb0ffe1c8232a30ce7951a34224288b749
Administrator <hbxglong@163.com>
2016-05-20 17:19:53 +0800

    增加差值动画效果3

--- app/src/main/java/com/hloong/androidherotest/activity/AniMoreActivity.java
+++ app/src/main/java/com/hloong/androidherotest/activity/AniMoreActivity.java
@@ -1,12 +1,18 @@
 package com.hloong.androidherotest.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hloong.androidherotest.R;
@@ -19,6 +25,13 @@ public class AniMoreActivity extends AppCompatActivity {
             R.id.imageView_d, R.id.imageView_e};
     private List<ImageView> mImageViews = new ArrayList<ImageView>();
     private boolean mFlag = true;
+
+    private TextView tvAni;
+
+
+    private LinearLayout mHiddenView,mShowView;
+    private float mDensity;
+    private int mHiddenViewMeasuredHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,18 +58,49 @@ public class AniMoreActivity extends AppCompatActivity {
             });
             mImageViews.add(imageView);
         }
+
+        tvAni = (TextView)findViewById(R.id.tvAniNo);
+        tvAni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+                valueAnimator.addUpdateListener(
+                        new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                tvAni.setText("$ " +(Integer) animation.getAnimatedValue());
+                            }
+                        });
+                valueAnimator.setDuration(3000);
+                valueAnimator.start();
+            }
+        });
+
+        mHiddenView = (LinearLayout) findViewById(R.id.hidden_view);
+        mShowView = (LinearLayout) findViewById(R.id.show_view);
+        // 获取像素密度
+        mDensity = getResources().getDisplayMetrics().density;
+        // 获取布局的高度
+        mHiddenViewMeasuredHeight = (int) (mDensity * 40 + 0.5);
+        mShowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mHiddenView.getVisibility() == View.GONE) {
+                    // 打开动画
+                    animateOpen();
+                } else {
+                    // 关闭动画
+                    animateClose();
+                }
+            }
+        });
     }
     private void closeAnim() {
-        ObjectAnimator animator0 = ObjectAnimator.ofFloat(mImageViews.get(0),
-                "alpha", 0.5F, 1F);
-        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mImageViews.get(1),
-                "translationY", 200F, 0);
-        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mImageViews.get(2),
-                "translationX", 200F, 0);
-        ObjectAnimator animator3 = ObjectAnimator.ofFloat(mImageViews.get(3),
-                "translationY", -200F, 0);
-        ObjectAnimator animator4 = ObjectAnimator.ofFloat(mImageViews.get(4),
-                "translationX", -200F, 0);
+        ObjectAnimator animator0 = ObjectAnimator.ofFloat(mImageViews.get(0),"alpha", 0.5F, 1F);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mImageViews.get(1),"translationY", 200F, 0);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mImageViews.get(2),"translationX", 200F, 0);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(mImageViews.get(3),"translationY", -200F, 0);
+        ObjectAnimator animator4 = ObjectAnimator.ofFloat(mImageViews.get(4),"translationX", -200F, 0);
         AnimatorSet set = new AnimatorSet();
         set.setDuration(500);
         set.setInterpolator(new BounceInterpolator());
@@ -66,37 +110,55 @@ public class AniMoreActivity extends AppCompatActivity {
     }
 
     private void startAnim() {
-        ObjectAnimator animator0 = ObjectAnimator.ofFloat(
-                mImageViews.get(0),
-                "alpha",
-                1F,
-                0.5F);
-        ObjectAnimator animator1 = ObjectAnimator.ofFloat(
-                mImageViews.get(1),
-                "translationY",
-                200F);
-        ObjectAnimator animator2 = ObjectAnimator.ofFloat(
-                mImageViews.get(2),
-                "translationX",
-                200F);
-        ObjectAnimator animator3 = ObjectAnimator.ofFloat(
-                mImageViews.get(3),
-                "translationY",
-                -200F);
-        ObjectAnimator animator4 = ObjectAnimator.ofFloat(
-                mImageViews.get(4),
-                "translationX",
-                -200F);
+        ObjectAnimator animator0 = ObjectAnimator.ofFloat(mImageViews.get(0),"alpha",1F,0.5F);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mImageViews.get(1),"translationY",200F);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mImageViews.get(2),"translationX",200F);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(mImageViews.get(3),"translationY",-200F);
+        ObjectAnimator animator4 = ObjectAnimator.ofFloat(mImageViews.get(4),"translationX",-200F);
         AnimatorSet set = new AnimatorSet();
         set.setDuration(500);
         set.setInterpolator(new BounceInterpolator());
-        set.playTogether(
-                animator0,
-                animator1,
-                animator2,
-                animator3,
-                animator4);
+        set.playTogether(animator0, animator1, animator2, animator3, animator4);
         set.start();
         mFlag = false;
+    }
+
+
+    private void animateOpen() {
+        mHiddenView.setVisibility(View.VISIBLE);
+        ValueAnimator animator = createDropAnimator(
+                mHiddenView,
+                0,
+                mHiddenViewMeasuredHeight);
+        animator.start();
+    }
+
+    private void animateClose() {
+        int origHeight = mHiddenView.getHeight();
+        ValueAnimator animator = createDropAnimator(mHiddenView, origHeight, 0);
+        animator.addListener(new AnimatorListenerAdapter() {
+            public void onAnimationEnd(Animator animation) {
+                mHiddenView.setVisibility(View.GONE);
+            }
+        });
+        animator.start();
+    }
+
+    private ValueAnimator createDropAnimator(
+            final View view, int start, int end) {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+        animator.addUpdateListener(
+                new ValueAnimator.AnimatorUpdateListener() {
+
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        int value = (Integer) valueAnimator.getAnimatedValue();
+                        ViewGroup.LayoutParams layoutParams =
+                                view.getLayoutParams();
+                        layoutParams.height = value;
+                        view.setLayoutParams(layoutParams);
+                    }
+                });
+        return animator;
     }
 }
